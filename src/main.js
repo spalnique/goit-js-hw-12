33ef49623a741b8c80743d18f80e9afdd6a004c7
Oleksii_S <lex.sydoruk@gmail.com>
2024-02-08 22:20:09 +0100

    .

--- src/main.js
+++ src/main.js
@@ -274,10 +274,10 @@ refs.form.addEventListener('submit', e => {
 
 // Код нижче був зроблений виключно у дослідницьких цілях
 
-refs.input.addEventListener('input', e => {
-  if (!/^[a-z\s]+$/gi.test(e.target.value)) {
-    setTimeout(() => {
-      e.target.value = e.target.value.slice(0, -1);
-    }, 100);
-  }
-});
+// refs.input.addEventListener('input', e => {
+//   if (!/^[a-z\s]+$/gi.test(e.target.value)) {
+//     setTimeout(() => {
+//       e.target.value = e.target.value.slice(0, -1);
+//     }, 100);
+//   }
+// });
